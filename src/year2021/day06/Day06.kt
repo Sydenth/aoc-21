b4a12c206308fb5d804a8ca8b97dbb56728bff54
Tobias Suchalla <tsuchalla@outlook.com>
2021-12-06 08:20:30 +0100

    - solved 2021, day 6

---
+++ src/year2021/day06/Day06.kt
@@ -0,0 +1,44 @@
+package year2021.day06
+
+import utils.PuzzleInput
+import utils.checkWithOutput
+
+fun main() {
+    val puzzleInput = PuzzleInput(2021, 6) { it }
+
+    val testInput = parseLine(puzzleInput.test.first())
+    checkWithOutput(dayXY_part1(testInput), 5934L)
+    checkWithOutput(dayXY_part2(testInput), 26984457539L)
+
+    val input = parseLine(puzzleInput.real.first())
+    println(dayXY_part1(input))
+    println(dayXY_part2(input))
+}
+
+fun parseLine(line: String): List<Int> = line.split(',').map(String::toInt)
+
+fun dayXY_part1(input: List<Int>): Long {
+    return simulateFishGrowth(input, 80)
+}
+
+fun dayXY_part2(input: List<Int>): Long {
+    return simulateFishGrowth(input, 256)
+}
+
+fun simulateFishGrowth(input: List<Int>, days: Int): Long {
+    val ageMap = input
+        .groupBy { it }
+        .mapValues { (_, count) -> count.size.toLong() }
+        .toMutableMap()
+
+    repeat(days) {
+        val reproducing = ageMap[0] ?: 0
+        for (i in 0..8) {
+            ageMap[i] = ageMap[i + 1] ?: 0
+        }
+        ageMap.merge(6, reproducing, Long::plus)
+        ageMap[8] = reproducing
+    }
+
+    return ageMap.values.sum()
+}

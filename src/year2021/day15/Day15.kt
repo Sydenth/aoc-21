447307613cf90273816d8e545fe09d9b697c271c
Tobias Suchalla <tsuchalla@outlook.com>
2021-12-19 17:19:48 +0100

    solved day 15

--- src/year2021/day15/Day15.kt
+++ src/year2021/day15/Day15.kt
@@ -2,15 +2,17 @@ package year2021.day15
 
 import utils.PuzzleInput
 import utils.checkWithOutput
+import utils.printAll
+import utils.println
 
 fun main() {
-    val puzzleInput = PuzzleInput(2021, 15, ::State)
+    val puzzleInput = PuzzleInput(2021, 15, ::DensityMap)
 
-    checkWithOutput(day15_part1(puzzleInput.tests[0]), 315)
-//    checkWithOutput(day15_part2(puzzleInput.tests[0]), 1)
+    checkWithOutput(day15_part1(puzzleInput.tests[0]), 40)
+    checkWithOutput(day15_part2(puzzleInput.tests[0]), 315)
 
     println(day15_part1(puzzleInput.real))
-//    println(day15_part2(puzzleInput.real))
+    println(day15_part2(puzzleInput.real))
 }
 
 data class Node(
@@ -18,25 +20,42 @@ data class Node(
     val y: Int,
     val cost: Int,
     var prev: Node?,
-    var distance: Int
+    var distance: Long
 )
 
-class State(input: List<String>) {
-    val nodes = input.flatMapIndexed { y, line ->
+class DensityMap(input: List<String>) {
+    val tile: List<Node> = input.flatMapIndexed { y, line ->
         line.mapIndexed { x, c ->
-            Node(x, y, c.digitToInt(), null, Int.MAX_VALUE)
+            Node(x, y, c.digitToInt(), null, Long.MAX_VALUE)
         }
     }
 
-    val startNode = nodes.find { (x, y, _, _, _) -> x == 0 && y == 0 }!!.also { it.distance = 0 }
-    val endNode = nodes.find { (x, y, _, _, _) -> x == input.last().length - 1 && y == input.size - 1 }!!
+    val width = tile.maxOf { it.x } + 1
+    val height = tile.maxOf { it.y } + 1
+}
+
+class State(densityMap: DensityMap, tileCount: Int) {
+    val nodes = List(tileCount) { dy ->
+        List(tileCount) { dx ->
+            densityMap.tile.map { node ->
+                node.copy(
+                    x = node.x + dx * densityMap.width,
+                    y = node.y + dy * densityMap.height,
+                    cost = ((node.cost + dy + dx - 1) % 9) + 1
+                )
+            }
+        }.flatten()
+    }.flatten()
+
+    val startNode = nodes.first().also { it.distance = 0 }
+    val endNode = nodes.last()
 
     val visited = mutableSetOf<Node>()
     val pending = mutableSetOf(startNode)
 
     fun advance() {
         val node = pending.minByOrNull { it.distance }!!
-        val neighbors = nodes.getNeighbors(node).filter { it !in visited }
+        val neighbors = getNeighbors(node).filter { it !in visited }
 
         neighbors.forEach {
             val distance = node.distance + it.cost
@@ -51,20 +70,20 @@ class State(input: List<String>) {
         pending.remove(node)
         pending.addAll(neighbors)
     }
-}
 
-fun List<Node>.getNeighbors(node: Node): List<Node> = filter { (x, y, _, _, _) ->
-    y == node.y && (x == node.x - 1 || x == node.x + 1) || x == node.x && (y == node.y - 1 || y == node.y + 1)
-}
-
-private fun day15_part1(state: State): Int {
-    while (state.pending.isNotEmpty()) {
-        state.advance()
+    fun getNeighbors(node: Node): List<Node> = nodes.filter { (x, y, _, _, _) ->
+        y == node.y && (x == node.x - 1 || x == node.x + 1) || x == node.x && (y == node.y - 1 || y == node.y + 1)
     }
+}
 
+private fun day15_part1(densityMap: DensityMap): Long {
+    val state = State(densityMap, tileCount = 1)
+    while (state.pending.isNotEmpty()) state.advance()
     return state.endNode.distance
 }
 
-private fun day15_part2(state: State): Int {
-    return 0
+private fun day15_part2(densityMap: DensityMap): Long {
+    val state = State(densityMap, tileCount = 5)
+    while (state.pending.isNotEmpty()) state.advance()
+    return state.endNode.distance
 }
